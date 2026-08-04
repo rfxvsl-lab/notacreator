d2dba0883151fd45a5c72203380537f6fd081d2a
rfxvsl-lab <hilal.alhamdi22@gmail.com>
2026-08-05 02:30:47 +0700

    chore: wrap app providers with ChakraProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,15 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../theme';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <ChakraProvider theme={theme}>
+        {children}
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
